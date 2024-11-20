f99bf29a5dba4101c27b15503730a256450fd1d0
danyatheworst <danyatheworst@gmail.com>
2024-11-20 19:37:04 +0200

    chore: remove unused import statements

--- src/main/java/com/example/api_gateway/service/JwtService.java
+++ src/main/java/com/example/api_gateway/service/JwtService.java
@@ -1,13 +1,11 @@
 package com.example.api_gateway.service;
 
 import io.jsonwebtoken.Jwts;
-import io.jsonwebtoken.io.Decoders;
 import io.jsonwebtoken.security.Keys;
 import org.springframework.beans.factory.annotation.Value;
 import org.springframework.stereotype.Service;
 
 import javax.crypto.SecretKey;
-import java.security.Key;
 import java.util.Base64;
 
 @Service
